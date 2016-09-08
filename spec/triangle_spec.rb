5a43f95f06f7d474aaf1b42bbdf18a2551d828fe
Hisaki Akaza <TheRed@users.noreply.github.com>
2016-09-08 14:52:03 +0900

    同値分割法と境界値分析でテストケースを追加
    テストケースは以下のように作ってみた
    ・長さは全て整数に限定
    ・最短辺の長さが1〜4に限定
    ・他の2辺の長さを順番に増やしていき、
    　三角形の種類が変わる値を境界値としてテスト
    ・長さが0のテストも実施

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,8 +2,45 @@ require 'spec_helper'
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe 'Triangle' do
-  specify { expect(Triangle.new("2, 3, 4").judge).to eq "不等辺三角形ですね！" }
-  specify { expect(Triangle.new("2, 2, 1").judge).to eq "二等辺三角形ですね！" }
-  specify { expect(Triangle.new("1, 1, 1").judge).to eq "正三角形ですね！" }
-  specify { expect(Triangle.new("1, 2, 3").judge).to eq "三角形じゃないです＞＜" }
+  specify '不等辺三角形' do
+    expect(Triangle.new("2, 3, 4").judge).to eq "不等辺三角形ですね！"
+    expect(Triangle.new("3, 4, 5").judge).to eq "不等辺三角形ですね！"
+    expect(Triangle.new("3, 4, 6").judge).to eq "不等辺三角形ですね！"
+    expect(Triangle.new("4, 5, 6").judge).to eq "不等辺三角形ですね！"
+    expect(Triangle.new("4, 5, 8").judge).to eq "不等辺三角形ですね！"
+  end
+
+
+  specify '二等辺三角形' do
+    expect(Triangle.new("2, 2, 1").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("2, 2, 3").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("3, 3, 2").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("3, 3, 4").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("3, 3, 5").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("4, 4, 3").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("4, 4, 5").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("4, 4, 7").judge).to eq "二等辺三角形ですね！"
+    expect(Triangle.new("5, 5, 4").judge).to eq "二等辺三角形ですね！"
+  end
+
+  specify '正三角形' do
+    expect(Triangle.new("1, 1, 1").judge).to eq "正三角形ですね！"
+    expect(Triangle.new("2, 2, 2").judge).to eq "正三角形ですね！"
+    expect(Triangle.new("3, 3, 3").judge).to eq "正三角形ですね！"
+    expect(Triangle.new("4, 4, 4").judge).to eq "正三角形ですね！"
+  end
+
+  specify '三角形じゃない' do
+    expect(Triangle.new("1, 1, 2").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("1, 2, 3").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("2, 2, 4").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("2, 3, 5").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("3, 3, 6").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("3, 4, 7").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("4, 4, 8").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("4, 5, 9").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("0, 0, 0").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("0, 0, 1").judge).to eq "三角形じゃないです＞＜"
+    expect(Triangle.new("0, 1, 1").judge).to eq "三角形じゃないです＞＜"
+  end
 end
